a96003cb651e33fb7d2d2a19cb374cdefec94d07
Gardner Dickson <gardner.dickson@gmail.com>
2019-01-01 13:27:39 -0500

    Add day 7, part 2

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -4,6 +4,8 @@ import scala.io.Source
 
 object Day7 extends App {
 
+  private val numWorkers = 5
+
   val lines = Source.fromResource("day_7_input.txt").getLines().toList
   val answer1 = part1(lines)
   println(s"Part 1: $answer1")
@@ -14,23 +16,7 @@ object Day7 extends App {
     val pattern = "Step ([A-Z]) must be finished before step ([A-Z]) can begin.".r
     val edges = lines.map { case pattern(first, second) => (first, second) }
 
-    // Map of each step to the set of steps that come next
-    val adjacencyLists = mutable.Map[String, mutable.Set[String]]()
-    // Map of each step to the set of prerequisite steps
-    val prerequisiteLists = mutable.Map[String, mutable.Set[String]]()
-
-    for ((step, nextStep) <- edges) {
-      if (adjacencyLists.contains(step)) {
-        adjacencyLists(step) += nextStep
-      } else {
-        adjacencyLists(step) = mutable.Set(nextStep)
-      }
-      if (prerequisiteLists.contains(nextStep)) {
-        prerequisiteLists(nextStep) += step
-      } else {
-        prerequisiteLists(nextStep) = mutable.Set(step)
-      }
-    }
+    val (adjacencyLists, prerequisiteLists) = getStepMappings(edges)
 
     // Start with the instructions that have no instruction preceding them
     val startSteps = (Set(edges.map(_._1): _*) diff Set(edges.map(_._2): _*)).toList.sorted
@@ -59,8 +45,97 @@ object Day7 extends App {
     orderedSteps.mkString("")
   }
 
-  def part2(lines: List[String]): String = {
-    ""
+  private def getStepMappings(edges: List[(String, String)]): (mutable.Map[String, mutable.Set[String]], mutable.Map[String, mutable.Set[String]]) = {
+    // Map of each step to the set of steps that come next
+    val adjacencyLists = mutable.Map[String, mutable.Set[String]]()
+    // Map of each step to the set of prerequisite steps
+    val prerequisiteLists = mutable.Map[String, mutable.Set[String]]()
+    for ((step, nextStep) <- edges) {
+      if (adjacencyLists.contains(step)) {
+        adjacencyLists(step) += nextStep
+      } else {
+        adjacencyLists(step) = mutable.Set(nextStep)
+      }
+      if (prerequisiteLists.contains(nextStep)) {
+        prerequisiteLists(nextStep) += step
+      } else {
+        prerequisiteLists(nextStep) = mutable.Set(step)
+      }
+    }
+    (adjacencyLists, prerequisiteLists)
+  }
+
+
+  def part2(lines: List[String]): Int = {
+    val pattern = "Step ([A-Z]) must be finished before step ([A-Z]) can begin.".r
+    val edges = lines.map { case pattern(first, second) => (first, second) }
+
+    val (adjacencyLists, prerequisiteLists) = getStepMappings(edges)
+
+    val startSteps = (Set(edges.map(_._1): _*) diff Set(edges.map(_._2): _*)).toList.sorted
+
+    val completed = mutable.Set[String]()
+    var remainingSteps = ListBuffer[String](startSteps: _*)
+    var runningSteps = ListBuffer[Step]()
+    var totalTicks = 0
+    while (remainingSteps.nonEmpty || runningSteps.nonEmpty) {
+      var remainingStepIndex = 0
+      while (remainingStepIndex < remainingSteps.length) {
+        val step = remainingSteps(remainingStepIndex)
+        if (!completed.contains(step) && !runningSteps.map(_.name).toSet[String].contains(step) && prerequisiteLists.getOrElse(step, completed).subsetOf(completed) && runningSteps.size < numWorkers) {
+          runningSteps += Step(step)
+          runningSteps = runningSteps.sorted
+          remainingSteps.remove(remainingStepIndex)
+          remainingStepIndex -= 1
+        }
+        remainingStepIndex += 1
+      }
+
+      var runningStepIndex = 0
+      while (runningStepIndex < runningSteps.length) {
+        val runningStep = runningSteps(runningStepIndex)
+        runningStep.tick()
+        if (runningStep.isDone) {
+          runningSteps.remove(runningStepIndex)
+          runningStepIndex -= 1
+          completed += runningStep.name
+          remainingSteps ++= adjacencyLists.getOrElse(runningStep.name, Set.empty[String])
+          remainingSteps = ListBuffer(remainingSteps.toSet.toList.sorted: _*)
+        }
+        runningStepIndex += 1
+      }
+
+      totalTicks += 1
+    }
+
+    totalTicks
+  }
+
+
+  object Step {
+    private val characterCodeOffset = 64
+  }
+
+  case class Step(name: String) extends Ordered[Step] {
+
+    private var ticks: Int = 0
+    private val totalDuration: Int = 60 + (name.toCharArray.head.toInt - Step.characterCodeOffset)
+    private var done: Boolean = false
+
+    def tick(): Int = {
+      if (done) throw new IllegalStateException("")
+      ticks += 1
+      if (ticks - totalDuration == 0) {
+        done = true
+      }
+      ticks
+    }
+
+    def isDone: Boolean = totalDuration - ticks == 0
+
+    override def compare(that: Step): Int = this.name compare that.name
+
+    override def toString: String = this.name
   }
 
 }
