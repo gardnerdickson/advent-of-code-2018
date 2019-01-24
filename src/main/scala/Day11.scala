3fdfc9f95f4e0964f3cb05ac163a97605be259d9
Gardner Dickson <gardner.dickson@gmail.com>
2019-01-24 15:58:45 -0500

    Add day 11, part 2

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -7,50 +7,68 @@ object Day11 extends App {
   println(s"Part 2: $answer2")
 
   def part1(gridSerialNumber: Int): Coordinate = {
-    val grid = Array.fill[Int](300, 300)(0)
-
-    val rows = 300
-    val cols = 300
-
-    for (y <- 1 to rows) {
-      for (x <- 1 to cols) {
-        val rackId = x + 10
-        var powerLevel = rackId * y
-        powerLevel += gridSerialNumber
-        powerLevel *= rackId
-        powerLevel = ((powerLevel / 100) % 10) - 5
-        grid(y - 1)(x - 1) = powerLevel
-      }
-    }
-
+    val grid = populateGrid(gridSerialNumber)
     var maxPowerLevel = Int.MinValue
     var maxPowerLevelCoordinate: Coordinate = (-1, -1)
     for (y <- grid.indices) {
       for (x <- grid(y).indices) {
-        val squarePowerLevel = calculateSquarePower(grid, (x, y))
+        val squarePowerLevel = calculateSquarePower(grid, (x, y), 3)
         if (squarePowerLevel > maxPowerLevel) {
           maxPowerLevel = squarePowerLevel
           maxPowerLevelCoordinate = (x + 1, y + 1)
         }
       }
     }
-
     maxPowerLevelCoordinate
   }
 
 
-  case class Coordinate(x: Int, y: Int)
+  def part2(gridSerialNumber: Int): (Int, Int, Int) = {
+    val grid = populateGrid(gridSerialNumber)
+    var maxPowerLevel = Int.MinValue
+    var maxPowerLevelCoordinate = (-1, -1, -1)
+    for (squareSize <- 1 to 300) {
+      for (y <- grid.indices) {
+        for (x <- grid(y).indices) {
+          val squarePowerLevel = calculateSquarePower(grid, (x, y), squareSize)
+          if (squarePowerLevel > maxPowerLevel) {
+            maxPowerLevel = squarePowerLevel
+            maxPowerLevelCoordinate = (x + 1, y + 1, squareSize)
+          }
+        }
+      }
+    }
+    maxPowerLevelCoordinate
+  }
 
-  implicit def tupleToCoordinate(tuple: (Int, Int)): Coordinate = Coordinate(tuple._1, tuple._2)
 
-  private def calculateSquarePower(grid: Array[Array[Int]], startingCoordinate: Coordinate): Int = {
-    if (startingCoordinate.x + 3 >= grid.length || startingCoordinate.y + 3 >= grid.length) Int.MinValue
-    val subGrid = grid.slice(startingCoordinate.y, startingCoordinate.y + 3).map(row => row.slice(startingCoordinate.x, startingCoordinate.x + 3))
+  private def populateGrid(serialNumber: Int): Array[Array[Int]] = {
+    val grid = Array.fill[Int](300, 300)(0)
+    val rows = 300
+    val cols = 300
+    for (y <- 1 to rows) {
+      for (x <- 1 to cols) {
+        val rackId = x + 10
+        var powerLevel = rackId * y
+        powerLevel += serialNumber
+        powerLevel *= rackId
+        powerLevel = ((powerLevel / 100) % 10) - 5
+        grid(y - 1)(x - 1) = powerLevel
+      }
+    }
+    grid
+  }
+
+
+  private def calculateSquarePower(grid: Array[Array[Int]], startingCoordinate: Coordinate, squareSize: Int): Int = {
+    if (startingCoordinate.x + squareSize >= grid.length || startingCoordinate.y + squareSize >= grid.length) Int.MinValue
+    val subGrid = grid.slice(startingCoordinate.y, startingCoordinate.y + squareSize).map(row => row.slice(startingCoordinate.x, startingCoordinate.x + squareSize))
     subGrid.flatten.sum
   }
 
-  def part2(gridSerialNumber: Int): Int = {
-    0
+  object Coordinate {
+    implicit def tupleToCoordinate(tuple: (Int, Int)): Coordinate = Coordinate(tuple._1, tuple._2)
   }
+  case class Coordinate(x: Int, y: Int)
 
 }
